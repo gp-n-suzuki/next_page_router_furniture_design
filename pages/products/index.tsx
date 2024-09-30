fd67de5a2432a078b8561d98cc74a31c0df73dcb
gp-n-suzuki <nobuaki_s@gp1000.jp>
2024-09-30 14:32:07 +0900

    Productsページ実装

---
+++ pages/products/index.tsx
@@ -0,0 +1,38 @@
+import Card from '@/components/Cards/Card'
+import Layout from '@/components/Layout/Layout'
+import React, { useEffect, useState } from 'react'
+import styles from '@/styles/products.module.css'
+import { Item } from '@/types/Item'
+import Paginator from '@/components/Paginator/Paginator'
+
+const Products = () => {
+  const [items, setItems] = useState<Item[]>([]);
+  const [totalPages, setTotalPages] = useState(0);
+  const [page, setPage] = useState(1);
+  const limit = 8;
+
+  useEffect(() => {
+    async function fetchItems() {
+      const response = await fetch(`/api/?page=${page}&limit=${limit}`);
+      const data = await response.json();
+      setItems(data.items);
+      setTotalPages(data.totalPages);
+    }
+
+    fetchItems();
+  }, [page]);
+
+  return (
+    <Layout>
+      <div className={styles.wrapper}>
+        <h2 className={styles.heading}>Products</h2>
+        <div className={styles.grid_container}>
+          <Card items={items} />
+        </div>
+        <Paginator totalPages={totalPages} setPage={setPage} />
+      </div>
+    </Layout>
+  )
+}
+
+export default Products
